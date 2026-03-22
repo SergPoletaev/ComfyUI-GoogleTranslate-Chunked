8fa15fa9d02f6b0dbfe36388c0c313cf9c9bc36f
SergPoletaev <105750518+SergPoletaev@users.noreply.github.com>
2026-03-23 02:07:45 +0300

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+ComfyUI Google Translate Chunked Node
+A custom node for translating long texts with automatic chunking.
+"""
+
+from .google_translate_chunked_node import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
